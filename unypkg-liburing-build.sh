e15554d96dec2c36ca6566d9baf948ca6f68c630
michacassola <mich@casso.la>
2024-08-17 15:07:01 +0200

    Update unypkg-liburing-build.sh

--- unypkg-liburing-build.sh
+++ unypkg-liburing-build.sh
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="liburing"
-pkggit="https://github.com/liburing/liburing.git refs/tags/*"
+pkggit="https://github.com/axboe/liburing.git refs/tags/*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "liburing-[0-9.]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "liburing-[0-9.].*" | sed "s|liburing-||")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
